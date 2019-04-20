648b6be3d59e7196bf14d409388a76ac6aceaa45
FranciscoFreitas45 <zefreitas2011@hotmail.com>
2019-04-20 17:43:43 +0100

    nave

---
+++ Fase3/build/CMakeFiles/generator.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/cyborg/CG/Fase3/GENERATOR/bezier.cpp" "/home/cyborg/CG/Fase3/build/CMakeFiles/generator.dir/GENERATOR/bezier.cpp.o"
+  "/home/cyborg/CG/Fase3/GENERATOR/box.cpp" "/home/cyborg/CG/Fase3/build/CMakeFiles/generator.dir/GENERATOR/box.cpp.o"
+  "/home/cyborg/CG/Fase3/GENERATOR/cone.cpp" "/home/cyborg/CG/Fase3/build/CMakeFiles/generator.dir/GENERATOR/cone.cpp.o"
+  "/home/cyborg/CG/Fase3/GENERATOR/cylinder.cpp" "/home/cyborg/CG/Fase3/build/CMakeFiles/generator.dir/GENERATOR/cylinder.cpp.o"
+  "/home/cyborg/CG/Fase3/GENERATOR/main.cpp" "/home/cyborg/CG/Fase3/build/CMakeFiles/generator.dir/GENERATOR/main.cpp.o"
+  "/home/cyborg/CG/Fase3/GENERATOR/plane.cpp" "/home/cyborg/CG/Fase3/build/CMakeFiles/generator.dir/GENERATOR/plane.cpp.o"
+  "/home/cyborg/CG/Fase3/GENERATOR/sphere.cpp" "/home/cyborg/CG/Fase3/build/CMakeFiles/generator.dir/GENERATOR/sphere.cpp.o"
+  "/home/cyborg/CG/Fase3/GENERATOR/torus.cpp" "/home/cyborg/CG/Fase3/build/CMakeFiles/generator.dir/GENERATOR/torus.cpp.o"
+  "/home/cyborg/CG/Fase3/src/Action.cpp" "/home/cyborg/CG/Fase3/build/CMakeFiles/generator.dir/src/Action.cpp.o"
+  "/home/cyborg/CG/Fase3/src/Group.cpp" "/home/cyborg/CG/Fase3/build/CMakeFiles/generator.dir/src/Group.cpp.o"
+  "/home/cyborg/CG/Fase3/src/Point.cpp" "/home/cyborg/CG/Fase3/build/CMakeFiles/generator.dir/src/Point.cpp.o"
+  "/home/cyborg/CG/Fase3/src/Shape.cpp" "/home/cyborg/CG/Fase3/build/CMakeFiles/generator.dir/src/Shape.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Fase3/cmake-build-debug/CMakeFiles/engine.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/cyborg/CG/Fase3/Engine/main.cpp" "/home/cyborg/CG/Fase3/cmake-build-debug/CMakeFiles/engine.dir/Engine/main.cpp.o"
+  "/home/cyborg/CG/Fase3/Engine/tinyxml2.cpp" "/home/cyborg/CG/Fase3/cmake-build-debug/CMakeFiles/engine.dir/Engine/tinyxml2.cpp.o"
+  "/home/cyborg/CG/Fase3/src/Action.cpp" "/home/cyborg/CG/Fase3/cmake-build-debug/CMakeFiles/engine.dir/src/Action.cpp.o"
+  "/home/cyborg/CG/Fase3/src/Group.cpp" "/home/cyborg/CG/Fase3/cmake-build-debug/CMakeFiles/engine.dir/src/Group.cpp.o"
+  "/home/cyborg/CG/Fase3/src/Point.cpp" "/home/cyborg/CG/Fase3/cmake-build-debug/CMakeFiles/engine.dir/src/Point.cpp.o"
+  "/home/cyborg/CG/Fase3/src/Shape.cpp" "/home/cyborg/CG/Fase3/cmake-build-debug/CMakeFiles/engine.dir/src/Shape.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
