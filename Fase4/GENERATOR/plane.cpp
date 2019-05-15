a4d1d375c86e9d163eb03687cca38bca1534cdc2
mariadias143 <marianovadias@gmail.com>
2019-05-15 20:22:14 +0100

    cmake

--- Fase4/GENERATOR/plane.cpp
+++ Fase4/GENERATOR/plane.cpp
@@ -12,29 +12,29 @@ Shape* plane(float size){
     //1
     s->insertPoint(new Point(-m,0,m));
     s->insertNormal(new Point(0,1,0));
-    s->insertTexture(new Point(0,0,0));
+    s->insertTexture(new Point(0,1,0));
     
     s->insertPoint(new Point(m,0,m));
     s->insertNormal(new Point(0,1,0));
-    s->insertTexture(new Point(1,0,0));
+    s->insertTexture(new Point(1,1,0));
     
     s->insertPoint(new Point(m,0,-m));
     s->insertNormal(new Point(0,1,0));
-    s->insertTexture(new Point(1,1,0));
+    s->insertTexture(new Point(1,0,0));
 
     //2
     s->insertPoint(new Point(-m,0,m));
     s->insertNormal(new Point(0,1,0));
-    s->insertTexture(new Point(0,0,0));
+    s->insertTexture(new Point(0,1,0));
     
     s->insertPoint(new Point(m,0,-m));
     s->insertNormal(new Point(0,1,0));
-    s->insertTexture(new Point(1,1,0));
+    s->insertTexture(new Point(1,0,0));
     
 
     s->insertPoint(new Point(-m,0,-m));
     s->insertNormal(new Point(0,1,0));
-    s->insertTexture(new Point(0,1,0));
+    s->insertTexture(new Point(0,0,0));
     return s;
 }
 
